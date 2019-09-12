ba83272f271882e285a3d84cda8fc10e96157ba6
adrianth2 <adrian_h3@hotmail.com>
2019-09-11 21:05:37 -0500

    step1

--- app/src/main/java/com/example/basurapk/infoRutas.java
+++ app/src/main/java/com/example/basurapk/infoRutas.java
@@ -3,13 +3,12 @@ package com.example.basurapk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class infoRutas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_info_rutas);
     }
 }
