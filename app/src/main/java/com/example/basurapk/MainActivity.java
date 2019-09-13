8a3f45498d5057980338486a80313ed0b8aff094
adrianth2 <adrian_h3@hotmail.com>
2019-09-13 10:56:04 -0500

    step6

--- app/src/main/java/com/example/basurapk/MainActivity.java
+++ app/src/main/java/com/example/basurapk/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.basurapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+      Button btnInfoRuta=(Button)findViewById(R.id.btnInfoRuta);
+
+      btnInfoRuta.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+
+              Intent intent = new Intent (view.getContext(), infoRutas.class);
+              startActivityForResult(intent, 0);
+
+          }
+      });
 
     }
 }
