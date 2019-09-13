27fe4b0562ebf615e2c95c7f716a56f39bd897cb
FlavioRA <flaviocesarrobledoayala@hotmail.com>
2019-09-13 11:10:11 -0500

    beto pui pui

--- app/src/main/java/com/example/basurapk/infoRutas.java
+++ app/src/main/java/com/example/basurapk/infoRutas.java
@@ -1,14 +1,34 @@
 package com.example.basurapk;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class infoRutas extends AppCompatActivity {
+
+
+    Button Return;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_rutas);
+
+        Return = (Button)findViewById(R.id.AtrasInfo);
+
+        Return.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent Return = new Intent(infoRutas.this, MainActivity.class);
+                startActivity(Return);
+            }
+
+        });
     }
+
+
 }
