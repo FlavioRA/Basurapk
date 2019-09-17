ba5e17259583ab20ec3085c3229872ce7c107ab9
FlavioRA <flaviocesarrobledoayala@hotmail.com>
2019-09-17 10:14:52 -0500

    ee

--- app/src/main/java/com/example/basurapk/infoRutas.java
+++ app/src/main/java/com/example/basurapk/infoRutas.java
@@ -12,23 +12,7 @@ public class infoRutas extends AppCompatActivity {
 
     Button Return;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_rutas);
 
-        Return = (Button)findViewById(R.id.AtrasInfo);
-
-        Return.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Intent Return = new Intent(infoRutas.this, MainActivity.class);
-                startActivity(Return);
-            }
-
-        });
-    }
 
 
 }
