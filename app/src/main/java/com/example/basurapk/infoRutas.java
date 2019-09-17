259bd8a2b2113d8ebe82b833215506b8fc04a6f2
FlavioRA <flaviocesarrobledoayala@hotmail.com>
2019-09-17 12:26:03 -0500

    ee

--- app/src/main/java/com/example/basurapk/infoRutas.java
+++ app/src/main/java/com/example/basurapk/infoRutas.java
@@ -1,14 +1,33 @@
 package com.example.basurapk;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class infoRutas extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_rutas2);
+
+        Button b = (Button) findViewById(R.id.btnR1);
+
+        b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+
+            public void onClick(View v) {
+                startActivity(new Intent(infoRutas.this,pop_up1.class));
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/basurapk/pop_up1.java
@@ -0,0 +1,27 @@
+package com.example.basurapk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class pop_up1 extends Activity {
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop_up1);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+    }
+}
+
+
