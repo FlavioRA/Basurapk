efe1d2110d354dfd435248a786c52cbb212500c1
adrianth2 <adrian_h3@hotmail.com>
2019-09-17 10:08:35 -0500

    step13

--- app/src/main/java/com/example/basurapk/contactoL.java
+++ app/src/main/java/com/example/basurapk/contactoL.java
@@ -2,7 +2,10 @@ package com.example.basurapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class contactoL extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class contactoL extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto_l);
+
+
+        ImageView imageView2 = (ImageView)findViewById(R.id.imageView2);
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent3 = new Intent(view.getContext(),MainActivity.class);
+                startActivityForResult(intent3, 0);
+
+            }
+        });
+
     }
 }
