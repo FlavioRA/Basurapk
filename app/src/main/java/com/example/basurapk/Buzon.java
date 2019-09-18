d5596d3a92eed91939bdea56b59487a117a16c16
adrianth2 <adrian_h3@hotmail.com>
2019-09-18 12:15:43 -0500

    step46

--- app/src/main/java/com/example/basurapk/Buzon.java
+++ app/src/main/java/com/example/basurapk/Buzon.java
@@ -2,7 +2,10 @@ package com.example.basurapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Buzon extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Buzon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buzon);
+
+
+        ImageView imglogo=(ImageView)findViewById(R.id.imglogo);
+
+
+        imglogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent3 = new Intent(view.getContext(),MainActivity.class);
+                startActivityForResult(intent3, 0);
+
+            }
+        });
+
+
+
+
     }
+
 }
