f651adbc87af17db8403f346d6a33e233adfaaf4
adrianth2 <adrian_h3@hotmail.com>
2019-09-18 11:25:50 -0500

    step16

--- app/src/main/java/com/example/basurapk/NuevaNotificacion.java
+++ app/src/main/java/com/example/basurapk/NuevaNotificacion.java
@@ -2,7 +2,10 @@ package com.example.basurapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NuevaNotificacion extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class NuevaNotificacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_notificacion);
+
+        ImageView imageView13 = (ImageView)findViewById(R.id.imageView13);
+
+        imageView13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent4 = new Intent(view.getContext(),MainActivity.class);
+                startActivityForResult(intent4, 0);
+
+            }
+        });
+
+
     }
 }
